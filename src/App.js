4d5feb2fed9cf0862df7897ab55799a0a68974fd
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-06-17 07:50:01 -0300

    Added StartMenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartMenu from './components/StartMenu';
 
 function App() {
   return (
     <div className="App">
-      <h2 class="mainTitle">Secret Word</h2>
+      <StartMenu />
     </div>
   );
 }
---
+++ src/components/StartMenu.js
@@ -0,0 +1,13 @@
+import './StartMenu.css';
+
+const StartMenu = () => {
+    return (
+        <>
+            <h2 className="mainTitle">Secret Word</h2>
+            <div>Click in the button below to start the game!</div>
+            <button className="startButton">Play Now!</button>
+        </>
+    )
+}
+
+export default StartMenu
